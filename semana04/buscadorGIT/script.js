9947c8258c5e714a8c8b5b0465f523339002ee47
DIEGO FERREIRA ARRUDA <diego_f_arruda@estudante.sesisenai.org.br>
2025-02-25 21:59:21 -0300

    25/02

---
+++ semana04/buscadorGIT/script.js
@@ -0,0 +1,29 @@
+async function loadprofile(){
+    const username = document.getElementById("buscarNome").value;
+    if(!username){
+        alert("Voçê precisa digitar o nome de um Usuario");
+        return;
+    }
+
+    const response = await fetch(`https://api.github.com/users/${username}`, {      /*fetch cria requisições*/
+        method:'GET'
+    })   
+
+    const user = await response.json(); 
+
+    const profiles = document.getElementById("profiles");
+    const profileElement = document.createElement("div");
+    profileElement.classList.add("profile-item");
+    profileElement.innerHTML = `
+        <img src= "${user.avatar_url}"/>
+        <h2>${user.name || user.login}</h2>
+        <p>${user.bio} </p>
+    `;
+
+    profiles.appendChild(profileElement)
+
+    console.log(user)
+
+}
+
+servicodados.ibge.gov.br/api/v2/censos/nomes/diego
