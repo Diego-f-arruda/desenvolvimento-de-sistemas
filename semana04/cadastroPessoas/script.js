11bd944ff2d00773d53e9c8cc158d51628e043f5
DIEGO FERREIRA ARRUDA <diego_f_arruda@estudante.sesisenai.org.br>
2025-02-27 21:41:09 -0300

    27/02

---
+++ semana04/cadastroPessoas/script.js
@@ -0,0 +1,33 @@
+async function addPerson(event){
+    event.preventDefaulth();
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("idade").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("telefone").value;
+    const address = document.getElementById("endereco").value;
+    const city = document.getElementById("cidade").value;
+
+    if(!name || !age || !email || !phone || !address || !city){
+        alert("Preencha todos os campos corretamente");
+        return;
+    }
+
+    const PERSON = {
+        name: name,
+        age: age,
+        email: email,
+        phone: phone,
+        address: address,
+        city:
+        city
+    }
+
+    await fetch("http://localhost:3000/person", {
+        method: 'POST',
+        body: JSON.stringify(PERSON), //STRINGFY consverte o conteudo para o formato json
+        headers: {'Content-Type': 'application/json'}
+
+    })
+
+    console.log(PERSON)
+}
